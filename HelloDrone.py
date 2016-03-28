8beb0e0ae3589d6b71c1d7127cbaabff8a05ac5f
VimDong <dongchongle@gmail.com>
2016-03-28 23:05:48 +0800

    Hello Drone

--- HelloDrone.py
+++ HelloDrone.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import urllib2
 
-response = urllib2.urlopen('http://www.baidu.com')
-html = response.read()
+request = urllib2.Request('http://www.baidu.com')
+response = urllib2.urlopen(request)
 
-print(html)
+print response.read()
